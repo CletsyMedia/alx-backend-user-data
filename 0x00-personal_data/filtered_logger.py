55cd2186960042fa3f6ae0f2bea263f3495049ad
CletsyMedia <Cletussam12@yahoo.com>
2024-05-30 00:02:20 +0100

    Logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,9 +2,11 @@
 """
 Module for handling Personal Data
 """
+import logging
 from typing import List
 import re
-import logging
+import mysql.connector
+from os import environ
 
 
 class RedactingFormatter(logging.Formatter):
@@ -79,3 +81,17 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
 
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Returns a connector to a MySQL database """
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
+
+    cnx = mysql.connector.connection.MySQLConnection(user=username,
+                                                     password=password,
+                                                     host=host,
+                                                     database=db_name)
+    return cnx
