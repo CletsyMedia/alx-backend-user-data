bac434ffa0224ac4d8b25eccb4b7ed24bd2d6dff
CletsyMedia <Cletussam12@yahoo.com>
2024-06-03 23:19:04 +0100

    Validating all request to secure the API

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,11 +13,38 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+# Import Auth if AUTH_TYPE is set to 'auth'
+if getenv("AUTH_TYPE") == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+else:
+    auth = None
+
+
+@app.before_request
+def before_request():
+    """Handler before each request"""
+    if auth is None:
+        return
+
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if request.path in excluded_paths:
+        return
+
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.current_user(request) is None:
+        abort(403)
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
+    """ Not found handler """
     return jsonify({"error": "Not found"}), 404
 
 
@@ -35,5 +62,5 @@ def forbidden(error) -> str:
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
+    port = int(getenv("API_PORT", "5000"))
     app.run(host=host, port=port)
