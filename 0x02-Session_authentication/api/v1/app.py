908e71cd3a3b8393a2c00178ceca6fadc41939b0
CletsyMedia <Cletussam12@yahoo.com>
2024-06-05 12:01:58 +0100

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,14 +32,17 @@ def before_request():
         return
 
     excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/', '/api/v1/forbidden/']
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if request.path in excluded_paths:
         return
 
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None:
+    # Check both authorization_header and session_cookie
+    if auth.authorization_header(
+            request) is None and auth.session_cookie(request) is None:
         abort(401)
 
     if auth.current_user(request) is None:
