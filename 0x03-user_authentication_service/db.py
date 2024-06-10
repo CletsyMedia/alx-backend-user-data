d0bdd9ef3929169bdd3041e3be92c35382735d17
CletsyMedia <Cletussam12@yahoo.com>
2024-06-10 08:11:58 +0100

    Completing the DB class

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+""" Database for ORM """
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from typing import TypeVar
+from user import Base, User
+
+
+class DB:
+    """ DB Class for Object Relational Mapping """
+
+    def __init__(self):
+        """ Constructor Method """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self):
+        """ Session Getter Method """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """ Adds user to database
+        Return: User Object
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Finds user by key word args
+        Return: First row found in the users table as filtered by kwargs
+        """
+        if not kwargs:
+            raise InvalidRequestError("No search criteria provided")
+
+        column_names = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in column_names:
+                raise InvalidRequestError(f"Invalid column name: {key}")
+
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+            return user
+        except NoResultFound:
+            return None
+
+
+if __name__ == "__main__":
+    my_db = DB()
+    user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
+    print(user_1.id)
+    user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
+    print(user_2.id)
